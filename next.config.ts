843fb237778d09b2fe0589240e388a27e5bbb55b
Imtiyaz-Rahman <imtiyazr@outlook.com>
2024-12-13 21:47:05 +0000

    Changes to match the repo path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Formative-Project-Currency-Slinger',
 };
 
 export default nextConfig;
